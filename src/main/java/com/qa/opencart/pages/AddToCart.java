4396ce0ad7726ca13f07dcd5e9d3e65613f204d6
gajendraksharma79 <gs.gajendraksharma@gmail.com>
2024-07-10 23:36:33 +0530

    added addToCart Page

---
+++ src/main/java/com/qa/opencart/pages/AddToCart.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+public class AddToCart {
+
+	String productToAdd = "iphone 13";
+	
+	public void addProduct() {
+		System.out.println("Product Added"+productToAdd);
+	}
+	
+}
